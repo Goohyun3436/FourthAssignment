bf09c4abab7c6a49a9b38e7cf85ce265fce5a484
Goohyun3436 <3436hyun@gmail.com>
2025-01-13 22:07:10 +0900

    add: 검색창 - 배경 UI

--- FourthAssignment/Views/Controllers/SearchViewController.swift
+++ FourthAssignment/Views/Controllers/SearchViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchViewController: UIViewController {
+    //MARK: - Property
+    let backgroundImageView = UIImageView()
 
+    //MARK: - Override Method
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
-        
         addSubviewBackButton()
+        configureLayout()
+        configureDesign()
+    }
+}
+
+//MARK: - Method
+extension SearchViewController {
+    func configureLayout() {
+        backgroundImageViewLayout()
+    }
+    
+    func configureDesign() {
+        view.backgroundColor = UIColor.black
+        backgroundImageViewDesign()
+    }
+}
+
+//MARK: - Layout
+extension SearchViewController {
+    func backgroundImageViewLayout() {
+        view.addSubview(backgroundImageView)
+        
+        backgroundImageView.snp.makeConstraints { make in
+            make.edges.equalTo(view)
+        }
+    }
+}
+
+//MARK: - Design
+extension SearchViewController {
+    func backgroundImageViewDesign() {
+        backgroundImageView.image = UIImage(named: "searchBackground")
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.alpha = 0.3
     }
 }
