bd5759e35b07e31d08e0d3c435ececcacf2704c5
Goohyun3436 <3436hyun@gmail.com>
2025-01-13 17:04:41 +0900

    add: 페이지 backButton UI 및 기능

--- FourthAssignment/Views/Controllers/SignUpViewController.swift
+++ FourthAssignment/Views/Controllers/SignUpViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class SignUpViewController: UIViewController {
+    //MARK: - Property
+    let backButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        view.backgroundColor = .black
+        
+        view.addSubview(backButton)
+        
+        backButton.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(8)
+            make.size.equalTo(44)
+        }
+        
+        backButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        backButton.tintColor = UIColor.white
+        
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func backButtonTapped() {
+        dismiss(animated: true)
     }
 }
