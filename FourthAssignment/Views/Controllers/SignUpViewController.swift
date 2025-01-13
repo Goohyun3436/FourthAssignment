f3130ac3920f972e11ece24cf3533619bdde0da6
Goohyun3436 <3436hyun@gmail.com>
2025-01-13 17:30:30 +0900

    add: 회원가입 - 로고 이미지 UI

--- FourthAssignment/Views/Controllers/SignUpViewController.swift
+++ FourthAssignment/Views/Controllers/SignUpViewController.swift
@@ -10,7 +10,7 @@ import SnapKit
 
 class SignUpViewController: UIViewController {
     //MARK: - Property
-    
+    let logoImageView = UIImageView()
 
     //MARK: - Override Method
     override func viewDidLoad() {
@@ -19,5 +19,40 @@ class SignUpViewController: UIViewController {
         view.backgroundColor = .black
         
         addSubviewBackButton()
+        configureLayout()
+        configureDesing()
+    }
+}
+
+//MARK: - Method
+extension SignUpViewController {
+    func configureLayout() {
+        logoImageViewLayout()
+    }
+    
+    func configureDesing() {
+        logoImageViewDesign()
+    }
+}
+
+//MARK: - Layout
+extension SignUpViewController {
+    func logoImageViewLayout() {
+        view.addSubview(logoImageView)
+        
+        logoImageView.snp.makeConstraints { make in
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(120)
+            make.width.equalTo(view.safeAreaLayoutGuide).multipliedBy(0.7)
+            make.height.equalTo(40)
+        }
+    }
+}
+
+//MARK: - Design
+extension SignUpViewController {
+    func logoImageViewDesign() {
+        logoImageView.image = UIImage(named: "gooflix")
+        logoImageView.contentMode = .scaleAspectFit
     }
 }
