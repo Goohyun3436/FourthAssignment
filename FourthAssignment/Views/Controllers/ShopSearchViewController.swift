1d9816b94c2e9e96a96139e7d9c815ad1ac704c5
Goohyun3436 <3436hyun@gmail.com>
2025-01-15 16:39:44 +0900

    add: 쇼핑 - 검색창 UI

--- FourthAssignment/Views/Controllers/ShopSearchViewController.swift
+++ FourthAssignment/Views/Controllers/ShopSearchViewController.swift
@@ -11,7 +11,7 @@ import SnapKit
 class ShopSearchViewController: UIViewController, ViewConfiguration {
     
     //MARK: - UI Property
-    let searchTextField = UISearchTextField()
+    let searchBar = UISearchBar()
     
     //MARK: - Override Method
     override func viewDidLoad() {
@@ -22,15 +22,17 @@ class ShopSearchViewController: UIViewController, ViewConfiguration {
         configureHierarchy()
         configureLayout()
         configureView()
+        
+        configureSearchBar()
     }
     
     //MARK: - Configure Method
     func configureHierarchy() {
-        view.addSubview(searchTextField)
+        view.addSubview(searchBar)
     }
     
     func configureLayout() {
-        searchTextField.snp.makeConstraints { make in
+        searchBar.snp.makeConstraints { make in
             make.horizontalEdges.equalToSuperview().inset(16)
             make.top.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(34)
@@ -40,7 +42,35 @@ class ShopSearchViewController: UIViewController, ViewConfiguration {
     func configureView() {
         view.backgroundColor = UIColor.systemBackground
         navigationItem.title = "도봉러의 쇼핑쇼핑"
-        searchTextField.placeholder = "브랜드, 상품, 프로필, 태그 등"
+    }
+    
+    func configureSearchBar() {
+        searchBar.delegate = self
+        searchBar.placeholder = "브랜드, 상품, 프로필, 태그 등"
+        searchBar.setValue("취소", forKey: "cancelButtonText")
+        searchBar.searchBarStyle = .minimal
+        searchBar.tintColor = UIColor.label
     }
 
+}
+
+//MARK: - UISearchTextField
+extension ShopSearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        searchBar.setShowsCancelButton(false, animated: true)
+        
+        print(#function)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        view.endEditing(true)
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
 }
