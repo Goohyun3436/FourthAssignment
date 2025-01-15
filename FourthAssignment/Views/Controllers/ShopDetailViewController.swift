538493ec2d421870b5a9e085cb0427b8868568dd
Goohyun3436 <3436hyun@gmail.com>
2025-01-15 17:39:16 +0900

    add: 쇼핑 - 총 검색 결과 수 UI

--- FourthAssignment/Views/Controllers/ShopDetailViewController.swift
+++ FourthAssignment/Views/Controllers/ShopDetailViewController.swift
@@ -6,16 +6,22 @@
 //
 
 import UIKit
+import SnapKit
 
 class ShopDetailViewController: UIViewController, ViewConfiguration {
     
     //MARK: - Property
     var searchText: String?
+    
+    //MARK: - UI Property
+    let totalLabel = UILabel()
 
     //MARK: - Override Method
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        navigationItem.title = searchText
+        
         configureHierarchy()
         configureLayout()
         configureView()
@@ -23,16 +29,22 @@ class ShopDetailViewController: UIViewController, ViewConfiguration {
     
     //MARK: - Configure Method
     func configureHierarchy() {
-        
+        view.addSubview(totalLabel)
     }
     
     func configureLayout() {
-        
+        totalLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+        }
     }
     
     func configureView() {
         view.backgroundColor = UIColor.systemBackground
-        navigationItem.title = searchText
+        
+        totalLabel.text = "13,234,448 개의 검색 결과"
+        totalLabel.font = UIFont.systemFont(ofSize: 12, weight: .bold)
+        totalLabel.textColor = UIColor.systemGreen
     }
 
 }
